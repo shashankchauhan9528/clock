9470cd6b82bd9853c6c24d839af6b1277f31815d
Shashank Chauhan <shashankchauhan1332@gmail.com>
2023-10-17 19:37:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const dateString = now.toLocaleDateString('en-US', options);
+
+  document.getElementById('time').innerText = timeString;
+  document.getElementById('date').innerText = dateString;
+}
+
+setInterval(updateClock, 1000); // Update every second
+updateClock(); // Update immediately when the page loads
